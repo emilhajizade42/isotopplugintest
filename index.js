b5503eca2284dd723c29e4c2216d79d1608cf364
ellnara <6XP3XCG@code.edu.az>
2022-05-25 19:42:43 +0400

    taskggg

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 
-
+// izotopun tanidilmasi
   var $cardbox = $('.cardbox').isotope({
     itemSelector: '.card',
     layoutMode: 'fitRows'
   });
 
 
-  
-$cardbox.isotope({ filter: '.red' });
+ 
+let filterBtns = document.querySelectorAll("ul li");
+[...filterBtns].forEach(function (btn) {
+  btn.onclick  = function (e) {
+    console.log(this);
+    console.log(this.getAttribute("data-filter"));
+    $cardbox.isotope({ filter: this.getAttribute("data-filter") });
+  }
+})
 
-// filter .alkali OR .alkaline-earth items
-$cardbox.isotope({ filter: '.black' });
+
+
+
+// $cardbox.isotope({ filter: '.red' });
+
+// // filter .alkali OR .alkaline-earth items
+// $cardbox.isotope({ filter: '.black' });
 
 // filter .metal AND .transition items
 
 
-// show all items
-$cardbox.isotope({ filter: '*' });
-$cardbox.isotope({ filter: '.blue' });
-
-
-$("ul")
+// // show all items
+// $cardbox.isotope({ filter: '*' });
+// $cardbox.isotope({ filter: '.blue' });
